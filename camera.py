54eedebfbe709dee1123a5a44bfd731141960664
Andision <48558940+Andision@users.noreply.github.com>
2021-10-12 15:11:07 +0800

    Update camera.py

--- camera.py
+++ camera.py
@@ -1,7 +1,7 @@
 import subprocess,time,sys
 while(True):
   # os.system('python3 /home/pi/Desktop/pi.py')
-  p = subprocess.Popen('python3 /home/pi/Desktop/pi.py', stdin = sys.stdin,stdout = sys.stdout, stderr = sys.stderr, shell = True)
+  p = subprocess.Popen('python3 /home/pi/Desktop/FaceRecognition-Pi/pi.py', stdin = sys.stdin,stdout = sys.stdout, stderr = sys.stderr, shell = True)
   print('haha')
   time.sleep(300)
   p.kill()
